410de9435e1789209971af2dea97df5e94c80931
AlexTyler120 <atyl0218@uni.sydney.edu.au>
2024-09-01 00:53:34 +1000

    working refactored code for original simulated image and something weird going on with the polarised images.

--- Sem2/Polarisation Weighting/home/WeightingEstimate.py
+++ Sem2/Polarisation Weighting/home/WeightingEstimate.py
@@ -49,6 +49,7 @@ def clarity_both_imgs(img, w1_est, shift_val):
 
     deconvolved_image_w1 = sk.restoration.wiener(img, psf_w1, balance=0)
     deconvolved_image_w2 = sk.restoration.wiener(img, psf_w2, balance=0)
+
     plt.figure()
     plt.subplot(1, 2, 1)
     plt.imshow(deconvolved_image_w1, cmap='gray')
@@ -67,6 +68,43 @@ def clarity_both_imgs(img, w1_est, shift_val):
         return 1 - w1_est
     else:
         return w1_est
+    
+def clarity_both_imgs_w1w2(img, w1_est, w2_est, shift_val):
+    """
+    Comput image clarity with both estimations where not added to one and invert.
+    img: the deconvolved image
+    w1_est: the estimate of the weight
+    w2_est: the other estimate of the weight
+    shift_val: the shift value
+    """
+
+    psf_w1 = get_img_psf_w1_w2(w1_est, w2_est, shift_val)
+    psf_w2 = get_img_psf_w1_w2(w2_est, w1_est, shift_val)
+
+    if np.max(img) > 1:
+        img = img / np.max(img)
+
+    deconvolved_image_w1 = sk.restoration.wiener(img, psf_w1, balance=0)
+    deconvolved_image_w2 = sk.restoration.wiener(img, psf_w2, balance=0)
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(deconvolved_image_w1, cmap='gray')
+    plt.title(f"Clarity: {clarity_loss(deconvolved_image_w1)}")
+    plt.subplot(1, 2, 2)
+    plt.imshow(deconvolved_image_w2, cmap='gray')
+    plt.title(f"Clarity: {clarity_loss(deconvolved_image_w2)}")
+
+    clarity_w1 = clarity_loss(deconvolved_image_w1)
+    clarity_w2 = clarity_loss(deconvolved_image_w2)
+
+    ## return the weight depending on which clarity is lower
+    print(f"Clarity w1: {clarity_w1} and Clarity w2: {clarity_w2}")
+    print(f"Weight w1: {w1_est} and Weight w2: {w2_est}")
+    if clarity_w2 < clarity_w1:
+        return w2_est, w1_est
+    else:
+        return w1_est, w2_est
 
 
 def check_gradients(corr_vals, shift_vals):
@@ -103,7 +141,7 @@ def check_flatness(shift_vals, fitlered_corr, shift_estimate):
 
     loss = 0
     # check the peak of shift vals
-    loss += 2*(abs(fitlered_corr[pos_idx]) + abs(fitlered_corr[neg_idx]))
+    loss += 20*(abs(fitlered_corr[pos_idx]) + abs(fitlered_corr[neg_idx]))
 
     # overall flatness from std
     CENTRAL_REG = 3
@@ -175,3 +213,84 @@ def optimise_psf(shifted_img, shift_val):
 
     return est_w1
 
+def get_img_psf_w1_w2(w1, w2, shift):
+    """
+    Get the image psf. That can be applied to the image for deconvolution.
+    w1: the weight of the psf
+    w2: the other weight of the psf
+    shift: the shift value
+    """
+    psf = np.zeros(shift + 1)
+    psf[-1] = w1
+    psf[0] = w2
+
+    # normalise
+    psf = psf / np.sum(psf)
+
+    psf = np.expand_dims(psf, axis=0)
+
+    return psf
+
+def loss_function_two_est(estimate, shifted_img, shift_val, loss_vals, w_vals):
+    """
+    Loss function to optimise the weights of the psf.
+    Just for one w1 and the other is 1 - w1
+    estimate: the estimate of the weight
+    shifted_img: the shifted image
+    shift_val: the shift value
+    """
+    w1_est, w2_est = estimate
+
+    if np.max(shifted_img) > 1:
+        shifted_img = shifted_img / np.max(shifted_img)
+
+    psf_estimate = get_img_psf_w1_w2(w1_est, w2_est, shift_val)
+
+    deconvolved_image = sk.restoration.wiener(shifted_img, psf_estimate, balance=0)
+
+    # normalise deconvolved image
+    deconvolved_img_norm = deconvolved_image / np.max(deconvolved_image)
+    
+    # obtain tyhe correlation values of the deconvolved image
+    shift_vals, corr_vals = ac.compute_auto_corr(deconvolved_img_norm, shift_val, shift_est_func=False, normalised=True)
+
+    # apply a savgol filter to the correlation values for peak detection
+    corr_filt = ac.obtain_peak_highlighted_curve(corr_vals)
+
+    # develop loss
+    loss = 0
+    loss = check_gradients(corr_filt, shift_vals)
+    loss += check_flatness(shift_vals, corr_filt, shift_val)
+
+    loss_vals.append(loss)
+    w_vals.append(estimate)
+
+    min_val = np.where(loss_vals == np.min(loss_vals))[0][0]
+
+    # print(f"Loss: {loss.min()} and w1: {w1_vals[min_val]}")
+
+    return loss
+
+def optimise_psf_both_weight(shifted_img, shift_val):
+    """
+    Want to optimsise both weights instead of just have w2 = 1 - w1
+    shifted_img: the shifted image
+    shift_val: the shift value
+    """
+    BOUNDS = [(0, 1), (0, 1)]
+    loss_vals = []
+    w_vals = []
+    result = sp.optimize.differential_evolution(loss_function_two_est, 
+                                                bounds=BOUNDS, 
+                                                args=(shifted_img, shift_val, loss_vals, w_vals),
+                                                disp=True,
+                                                polish=True, # use L-BFGS-B to polish the best result
+                                                workers=-1)
+    
+    w1_estimate = result.x[0]
+    w2_estimate = result.x[1]
+    loss_value = result.fun
+
+    est_w1, est_w2 = clarity_both_imgs_w1w2(shifted_img, w1_estimate, w2_estimate, shift_val)
+
+    return est_w1, est_w2
--- Sem2/Polarisation Weighting/home/main.py
+++ Sem2/Polarisation Weighting/home/main.py
@@ -2,21 +2,24 @@ import Images
 import ShiftEstimate
 import WeightingEstimate
 import matplotlib.pyplot as plt
+from skimage import restoration as sk
+import Autocorrelation as ac
+import numpy as np
 def main():
-    RESIZE_VAR = 0.5
+    RESIZE_VAR = 0.6
     GREY = False
     SIMULATED_SHIFT = 12
     WEIGHTING_SIM = 0.7
 
     ### Shift estimates with polarised images ###
-    # img1, _, _ = Images.read_image("python/test_im/ball/ball_0.png", RESIZE_VAR, GREY)
-    # img2, _, _ = Images.read_image("python/test_im/ball/ball_90.png", RESIZE_VAR, GREY)
-    # transformed_image = Images.create_shifted_image_polarised_imgs(img1, img2, SIMULATED_SHIFT, True)
+    img1, _, _ = Images.read_image("python/test_im/ball/ball_0.png", RESIZE_VAR, GREY)
+    img2, _, _ = Images.read_image("python/test_im/ball/ball_90.png", RESIZE_VAR, GREY)
+    transformed_image = Images.create_shifted_image_polarised_imgs(img1, img2, SIMULATED_SHIFT, True)
     ### ###
 
     ### Shift estimates with simulated images ###
-    img, _, _ = Images.read_image("python/test_im/bird.jpg", RESIZE_VAR, GREY)
-    transformed_image = Images.create_shifted_simulation(img, WEIGHTING_SIM, SIMULATED_SHIFT)
+    # img, _, _ = Images.read_image("python/test_im/flowers.jpg", RESIZE_VAR, GREY)
+    # transformed_image = Images.create_shifted_simulation(img, WEIGHTING_SIM, SIMULATED_SHIFT)
     ### ###
 
     shift_est = ShiftEstimate.compute_pixel_shift(transformed_image)
@@ -25,10 +28,28 @@ def main():
     for i in range(3):
         img_channel = transformed_image[:, :, i]
 
-        est_w1 = WeightingEstimate.optimise_psf(img_channel, shift_est)
+        # est_w1 = WeightingEstimate.optimise_psf(img_channel, shift_est)
+        est = WeightingEstimate.optimise_psf_both_weight(img_channel, shift_est)
 
-        w1_vals.append(est_w1)
-    print(w1_vals)
+        w1_vals.append(est)
+
+    for i in range(len(w1_vals)):
+        print(f"Channel {i}: {w1_vals[i]}")
+        plt.figure()
+        plt.subplot(1, 3, 1)
+        image = transformed_image[:, :, i] / np.max(transformed_image[:, :, i])
+        deconvolved = sk.wiener(image, WeightingEstimate.get_img_psf_w1_w2(w1_vals[i][0], w1_vals[i][1], shift_est), balance=0)
+        # deconvolved between 0 -255
+        plt.imshow(deconvolved, cmap='gray')
+        plt.title(f"Channel {i} Deconvolved")
+        plt.subplot(1, 3, 2)
+        shift, corr = ac.compute_auto_corr(deconvolved, shift_est)
+        plt.plot(shift, corr)
+        plt.title(f"Channel {i} Auto Correlation")
+        plt.subplot(1, 3, 3)
+        plt.plot(shift, ac.obtain_peak_highlighted_curve(corr))
+        plt.title(f"Channel {i} Filtered Auto Correlation")
+        
     plt.show()
 if __name__ == "__main__":
     main()
